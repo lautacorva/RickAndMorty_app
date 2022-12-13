718cc5784ba791611a6f3202ee3bfcfe03d7c508
Laucha <lautacorva69@gmail.com>
2022-12-13 12:35:43 -0300

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,10 +3,15 @@ import style from './About.module.css'
 export default function About() {
 	return (
 		<div className={style.container}>
-			<h1>Hola! Mi nombre es Lautaro</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et minus repudiandae velit nulla. Totam amet beatae ea esse dolore rem tempore blanditiis assumenda mollitia, animi molestias dolores eveniet maxime distinctio?
-			<br />
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde magnam ab minima porro sit fuga aliquid cum dolore corporis eos nemo, a dolores velit! Id dicta excepturi tempore vero delectus!
+			<h1 style={{fontSize: '25px'}}>Hola! Mi nombre es Lautaro Corva</h1>
+			<p>Esta sección está reservada para comentarle a usted, señor usuario, los detalles del desarollo de esta sencilla página web.
+				"RICK AND MORTY app" es un proyecto individual e integrador que fue realizado (aún sin terminar) durante el cursado del Módulo 2 en el bootcamp <strong>#SoyHenry</strong>. Está realizado enteramente con <strong>REACT</strong>, haciendo uso de la herramienta <strong>REDUX</strong> y mostrando mis vagos conocimientos en CSS (no es para nada mi fuerte, sepan disculpar!) 
+			<hr />
+				El principal objetivo detrás de esta aplicación web es poder demostrar mis conocimientos en <strong>FRONTEND</strong> con las ya mencionadas herramientas empleadas. Recuerde que aún sigo aprendiendo!
+			<hr />
+				Mi idea desde un principio fue tratar de respetar la paleta de colores de la serie, así como su estilo "infantil". Como verá, estoy lejos de terminar la página. Es cierto, podría acomodar todo de una manera más minimalista para tener así el proyecto listo para presentar... pero eso no sería tan divertido. Tengo un día a día ocupado, y es por esto que el avance es lento, aunque de todas maneras, este proyecto no tiene mucho tiempo de iniciado, y confío plenamente en mis habilidades para finalizarlo.
+			<hr />
+				Muchas gracias por detenerse a leer esto! Saludos señor usuario :)
 			</p>
 		</div>
 	);
